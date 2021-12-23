b78a74f70406dbf01d660ddb0d3e82a7ceda7ea3
tartarus-git <niktedig@gmail.com>
2021-12-23 21:15:13 +0100

    Finished documenting, finished formatting, finished error code sorting, working on unfinished TODO's. As a result of this, Error enum isn't totally perfect yet, one number doesn't fit in.

--- Camera.cpp
+++ Camera.cpp
@@ -17,16 +17,13 @@
 
 #include <linux/videodev2.h>
 
-// TODO: See if you can change contents of const char* by casting to char*
-const char* Camera::getErrorMessage(Error cameraError, int errnoValue) {
-	switch (cameraError) {
-	case Error::none: return nullptr;
-	case Error::status_info_unavailable: return "failed to get device file status info";
-	case Error::file_is_not_device: return "the specified file does not represent a device";
-					// TODO: etc...
-	}
-}
+// SIDE-NOTE: Can you modify a string literal? They're expressed as const char*, but (through casting to char*) can you modify them?
+// Answer: No, you can't. It would work out syntactically and the compiler would let you, but it isn't possible because string literals are stored in read-only memory.
+// Accessing them, even if C++ lets you, is undefined behaviour. I can imagine that it will probably end in a seg fault.
+// Fun fact: char*="x" is also a pointer to string literal, so changing any part of that results in undefined behaviour, even though the language very easily lets you.
 
+// Does the same thing as ioctl, but recovers from signals interrupting the syscall by running the syscall over and over until it doesn't report an interrupt.
+// "over and over" will basically never happen. If it doesn't succeed on the first try, it probably will on the second.
 int interruptedIoctl(int fd, unsigned long request, void* argp) {
 	int returnValue;
 	do { returnValue = ioctl(fd, request, argp); }
@@ -46,18 +43,19 @@ Camera::Camera(const char* deviceName) : deviceName(deviceName) {
 	pollStruct.events = POLLIN;
 }
 
-Camera& Camera::operator=(Camera&& other) {			// TODO: Make sure all necessary fields are getting copied here and you didn't forget anything.
+Camera& Camera::operator=(Camera&& other) {
 	deviceName = other.deviceName;
 	fd = other.fd;
 	capabilities = other.capabilities;
 	format = other.format;
 	bufferMetadata = other.bufferMetadata;
+	lastBufferIndex = other.lastBufferIndex;
 	bufferData = other.bufferData;
 	streamingParameters = other.streamingParameters;
 
 	frameLocations = other.frameLocations;
 
-	initialized = false;
+	other.initialized = false;
 	other.fd = -1;
 
 	return *this;
@@ -69,9 +67,12 @@ Camera::Error Camera::open() {
 	struct stat st;					// I think struct is necessary here because compiler interprets as stat() function otherwise.
 	if (stat(deviceName, &st) == -1) { return Error::status_info_unavailable; }
 	if (!S_ISCHR(st.st_mode)) { return Error::file_is_not_device; }
+
 	fd = ::open(deviceName, O_RDWR | O_NONBLOCK);
 	if (fd == -1) { return Error::file_open_failed; }
+
 	pollStruct.fd = fd;
+
 	return Error::none;
 }
 
@@ -87,15 +88,12 @@ Camera::Error Camera::readPreferredFormat() {
 	return Error::none;
 }
 
-Camera::Error Camera::tryFormat() {
-	if (interruptedIoctl(fd, VIDIOC_TRY_FMT, &format) == -1) { return Error::device_format_unavailable; }
-	return Error::none;
-}
+Camera::Error Camera::tryFormat() { if (interruptedIoctl(fd, VIDIOC_TRY_FMT, &format) == -1) { return Error::device_format_unavailable; } return Error::none; }
 
 Camera::Error Camera::init() {
 	if (initialized) { return Error::not_freed; }			// don't allow initialization before freeing previous initialization
 
-	// See if the camera supports capture and streaming. Necessary for capturing images through mmap.
+	// See if the camera supports capture and streaming. Necessary for getting images through mmap.
 
 	Error err = readCapabilities();
 	if (err != Error::none) { return err; }
@@ -110,7 +108,7 @@ Camera::Error Camera::init() {
 	if (memcmp(&format, &previousFormatState, sizeof(v4l2_format)) != 0) { return Error::format_unsupported; }
 	// If the check didn't fail, the format was set on the device, so we can continue.
 
-	// initialize device buffers and set up shared memory access
+	// initialize device buffers and user data and set up shared memory access
 
 	if (bufferMetadata.count == 0) { bufferMetadata.count = 1; }
 	if (interruptedIoctl(fd, VIDIOC_REQBUFS, &bufferMetadata) == -1) { return Error::device_buffer_request_failed; }
@@ -119,22 +117,25 @@ Camera::Error Camera::init() {
 	frameLocations = (BufferLocation*)calloc(bufferMetadata.count, sizeof(BufferLocation));
 	if (!frameLocations) { err = Error::user_out_of_memory; goto freeDeviceBuffersAndReturnError; }
 
+	// TODO: See if for loop skips the first, useless comparison, or if we should use something else. Does it get optimized?
 	for (bufferData.index = 0; bufferData.index < bufferMetadata.count; bufferData.index++) {
 		if (interruptedIoctl(fd, VIDIOC_QUERYBUF, &bufferData) == -1) { err = Error::device_buffer_query_failed; goto freeAndReturnError; }
-
 		frameLocations[bufferData.index].start = mmap(nullptr, bufferData.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, bufferData.m.offset);
 		if (frameLocations[bufferData.index].start == MAP_FAILED) { err = Error::mmap_failed; goto freeAndReturnError; }
 		frameLocations[bufferData.index].size = bufferData.length;
 		frameLocations[bufferData.index].queued = false;
 	}
-
+	bufferData.index = 0;			// We do this so that queueFrame has a good starting point.
+						// We also HAVE to change it because without this line, bufferData.index equals bufferMetadata.count + 1.
 	initialized = true;
 	return Error::none;
 
 freeAndReturnError:
 	// try to free the mmaps that didn't fail
 	for (uint32_t i = 0; i < bufferData.index; i++) { munmap(frameLocations[i].start, frameLocations[i].size); }	// no need to handle error here
+
 	delete[] frameLocations;
+
 freeDeviceBuffersAndReturnError:
 	bufferMetadata.count = 0;
 	interruptedIoctl(fd, VIDIOC_REQBUFS, &bufferMetadata);
@@ -142,8 +143,7 @@ freeDeviceBuffersAndReturnError:
 }
 
 Camera::Error Camera::init(uint32_t pixelFormat, uint32_t field) {
-	Error err = readPreferredFormat();
-	if (err != Error::none) { return err; }
+	Error err = readPreferredFormat(); if (err != Error::none) { return err; }
 	format.fmt.pix.pixelformat = pixelFormat;
 	format.fmt.pix.field = field;
 	return init();
@@ -159,11 +159,11 @@ Camera::Error Camera::readStreamingParameters() {
 }
 
 // NOTE: We would have to worry about the capture standard if we were supporting old devices, modern, digital devices (webcams, etc...) don't have v4l2 standards.
-// NOTE: Capture standards set a minimum timePerFrame, which, following the above, we don't have to worry about here.
-// NOTE: Device itself may limit (upper and lower) the timePerFrame, setting invalid values to their nearest valid values.
+// Capture standards set a minimum timePerFrame, which, following the above, we don't have to worry about here.
+// Device itself may limit (upper and lower) the timePerFrame, setting invalid values to their nearest valid values.
 Camera::Error Camera::setTimePerFrame(uint32_t numerator, uint32_t denominator) {
-	Error err = readStreamingParameters();
-	if (err != Error::none) { return err; }
+	Error err = readStreamingParameters(); if (err != Error::none) { return err; }
+
 	if (streamingParameters.parm.capture.capability & V4L2_CAP_TIMEPERFRAME) {
 		streamingParameters.parm.capture.timeperframe.numerator = numerator;
 		streamingParameters.parm.capture.timeperframe.denominator = denominator;
@@ -174,8 +174,7 @@ Camera::Error Camera::setTimePerFrame(uint32_t numerator, uint32_t denominator) 
 }
 
 Camera::Error Camera::getTimePerFrame(uint32_t& numerator, uint32_t& denominator) {
-	Error err = readStreamingParameters();
-	if (err != Error::none) { return err; }
+	Error err = readStreamingParameters(); if (err != Error::none) { return err; }
 	numerator = streamingParameters.parm.capture.timeperframe.numerator;
 	denominator = streamingParameters.parm.capture.timeperframe.denominator;
 	return Error::none;
@@ -187,22 +186,36 @@ Camera::Error Camera::start() {
 	return Error::none;
 }
 
-// NOTE: Put this somewhere nice-looking: You can queue all you want to, even before starting the stream, they just wont get processed before starting the stream.
+Camera::Error Camera::queueFrame() {
+	if (interruptedIoctl(fd, VIDIOC_QBUF, &bufferData) == -1) { return Error::device_queue_buffer_failed; }
+	frameLocations[bufferData.index].queued = true;
+	if (bufferData.index == lastBufferIndex) { bufferData.index = 0; return Error::none; }
+	bufferData.index++;
+	return Error::none;
+}
+
+// TODO: I think: if POLLERR is encountered, either you haven't started the stream or there is nothing queued and thereby nothing can be dequeued.
+// This is the behaviour that I think the docs are talking about, but you should defo test to see if those two scenarios get captured by what I have set as
+// stream_not_started at the moment. If that or statement is true, then I'll have to come up with a different error message for that I think.
+// You can potentially make a couple of the functions that have to do with queueing a lot better with this behaviour.
+
+Camera::Error Camera::dequeueFrame() {
+	if (poll(&pollStruct, 1, -1) == -1) { return Error::poll_failed; }
+	if (pollStruct.revents & POLLERR) { return Error::stream_not_started; }
+	if (interruptedIoctl(fd, VIDIOC_DQBUF, &bufferData) == -1) { return Error::device_dequeue_buffer_failed; }
+	frameLocations[bufferData.index].queued = false;
+	return Error::none;
+}
+
 Camera::Error Camera::queueAllFrames() {
-	for (bufferData.index = 0; bufferData.index < bufferMetadata.count; bufferData.index++) {
-		if (interruptedIoctl(fd, VIDIOC_QBUF, &bufferData) == -1) { return Error::device_queue_buffer_failed; }
-		frameLocations[bufferData.index].queued = true;
-	}
+	bufferData.index = 0;
+	Error err = queueFrame(); if (err != Error::none) { return err; }
+	while (bufferData.index != 0) { err = queueFrame(); if (err != Error::none) { return err; } }
 	return Error::none;
 }
 
 Camera::Error Camera::dequeueAllFrames() {
-	for (unsigned int i = 0; i < bufferMetadata.count; i++) {
-		if (poll(&pollStruct, 1, -1) == -1) { return Error::poll_failed; }
-		if (pollStruct.revents & POLLERR) { return Error::poll_failed; }		// TODO: Make sure this is necessary.
-		if (interruptedIoctl(fd, VIDIOC_DQBUF, &bufferData) == -1) { return Error::device_dequeue_buffer_failed; }
-		frameLocations[bufferData.index].queued = false;
-	}
+	for (uint32_t i = 0; i < bufferMetadata.count; i++) { Error err = dequeueFrame(); if (err != Error::none) { return err; } }
 	return Error::none;
 }
 
@@ -210,33 +223,19 @@ Camera::Error Camera::dequeueAllQueuedFrames() {
 	unsigned int extraQueuedBuffers = 0;
 	for (unsigned int i = 0; i < bufferMetadata.count; i++) {
 		if (frameLocations[i].queued) {
-			if (poll(&pollStruct, 1, -1) == -1) { return Error::poll_failed; }
-			if (pollStruct.revents & POLLERR) { return Error::poll_failed; }		// TODO: Make sure this is necessary.
-			if (interruptedIoctl(fd, VIDIOC_DQBUF, &bufferData) == -1) { return Error::device_dequeue_buffer_failed; }
-			if (bufferData.index == i) { frameLocations[i].queued = false; continue; }
-			frameLocations[bufferData.index].queued = false;
-			extraQueuedBuffers++;
+			Error err = dequeueFrame(); if (err != Error::none) { return err; }
+			if (!frameLocations[i].queued) { extraQueuedBuffers++; }
 		}
 	}
-	for (unsigned int i = 0; i < extraQueuedBuffers; i++) {
-		if (poll(&pollStruct, 1, -1) == -1) { return Error::poll_failed; }
-		if (pollStruct.revents & POLLERR) { return Error::poll_failed; }		// TODO: Make sure this is necessary.
-		if (interruptedIoctl(fd, VIDIOC_DQBUF, &bufferData) == -1) { return Error::device_dequeue_buffer_failed; }
-		frameLocations[bufferData.index].queued = false;
-	}
+	for (unsigned int i = 0; i < extraQueuedBuffers; i++) { Error err = dequeueFrame(); if (err != Error::none) { return err; } }
 	return Error::none;
 }
 
 Camera::Error Camera::shootFrame() {
-	Error err = dequeueAllQueuedFrames();
-	if (err != Error::none) { return err; }
+	Error err = dequeueAllQueuedFrames(); if (err != Error::none) { return err; }
 	bufferData.index = 0;
-	if (interruptedIoctl(fd, VIDIOC_QBUF, &bufferData) == -1) { return Error::device_queue_buffer_failed; }
-	frameLocations[0].queued = true;
-	if (poll(&pollStruct, 1, -1) == -1) { return Error::poll_failed; }
-	if (pollStruct.revents & POLLERR) { return Error::poll_failed; }
-	if (interruptedIoctl(fd, VIDIOC_DQBUF, &bufferData) == -1) { return Error::device_dequeue_buffer_failed; }
-	frameLocations[0].queued = false;
+	err = queueFrame(); if (err != Error::none) { return err; }
+	err = dequeueFrame(); if (err != Error::none) { return err; }
 	return Error::none;
 }
 
@@ -247,31 +246,33 @@ Camera::Error Camera::stop() {
 }
 
 Camera::Error Camera::free() {
-	if (bufferMetadata.count == 0) { return Error::none; }
+	if (!initialized) { return Error::already_freed; }
 
 	Error err = stop();
 	if (err != Error::none) { return err; }
 
 	for (uint32_t i = 0; i < bufferMetadata.count; i++) { if (munmap(frameLocations[i].start, frameLocations[i].size) == -1) { return Error::munmap_failed; } }
 	delete[] frameLocations;
-	// Temp code:
-	unsigned int tempCount = bufferMetadata.count;
+
 	bufferMetadata.count = 0;
-	if (interruptedIoctl(fd, VIDIOC_REQBUFS, &bufferData) == -1) { bufferMetadata.count = tempCount; if (errno == EINVAL) { return Error::device_mmap_unsupported; } return Error::device_request_buffers_failed; }
-	if (bufferMetadata.count != 0) { return Error::device_free_buffers_failed; }
+	if (interruptedIoctl(fd, VIDIOC_REQBUFS, &bufferMetadata) == -1) { if (errno == EINVAL) { return Error::device_mmap_unsupported; } return Error::device_request_buffers_failed; }
+
 	return Error::none;
 }
 
 Camera::Error Camera::close() {
 	if (fd == -1) { return Error::already_closed; }
+
 	// NOTE: The fact that free() frees the device buffers is useless here because closing fd does it anyway. I'm leaving it in free() to make calling free() directly
 	// possible. Making close() a special case, where freeing the buffers is avoided because of redundancy, isn't worth it in my opinion.
 	// Plus, it might not even improve performance or make the slightest amount of difference because the driver probably checks if the buffers are freed.
 	// If they are, like we're doing right now, the driver probably skips unnecessary work.
 	Error err = free();
 	if (err != Error::none) { return err; }
+
 	if (::close(fd) == -1) { fd = -1; return Error::file_close_failed; }
 	fd = -1;
+
 	return Error::none;
 }
 
--- Camera.h
+++ Camera.h
@@ -4,89 +4,59 @@
 
 #include <linux/videodev2.h>
 
-
 class Camera {
 public:
-	enum class Error {
+	enum class Error {			// TODO: These errors need to have the correct numbers, already sorted though.
 		none = 0,
-		statusInfo = -1,
-		fileNotDevice = -2,
-		cannotOpen = -3,
-		already_closed = -4,
-		file_close_failed = -4,
-		deviceQueryCap = -5,
-		deviceNoVideo = -6,
-		deviceNoStreaming = -7,
-		deviceGetFormat = -8,
-		deviceSetFormat = -9,
-		deviceFormatUnsupported = -10,
-		deviceNoMMap = -11,
-		deviceRequestBuffer = -12,
-		deviceInsufficientMemory = -13,
-		userInsufficientMemory = -14,
-		deviceQueryBuf = -15,
-		mMapFailed = -16,
-		mUnmapFailed = -17,
-		deviceMemFree = -18,
-		deviceGetParm = -19,
-		deviceSetParm = -20,
-		deviceNoCustomFPS = -21,
-		deviceStart = -22,
-		deviceStop = -23,
-		deviceShootQBuf = -24,
-		deviceShootDQBuf = -25,
-		userShootPoll = -26,
-		munmap_failed,
-		device_mmap_unsupported,
-		device_request_buffers_failed,
-		device_free_buffers_failed,
-		device_stop_failed,
-		device_queue_buffer_failed,
-		poll_failed,
-		device_dequeue_buffer_failed,
-		device_start_failed,
-		device_set_streaming_parameters_failed,
-		device_custom_timeperframe_unsupported,
-		device_streaming_parameters_unavailable,
-		device_format_unavailable,
-		not_freed,
-		device_video_capture_unsupported,
-		device_streaming_unsupported,
-		invalid_format_type,
-		invalid_format_type,
-		format_unsupported,
-		device_buffer_request_failed,
-		device_out_of_memory,
-		mmap_failed,
-		device_capabilities_unavailable,
-		device_buffer_query_failed,
-		user_out_of_memory,
-		file_open_failed,
-		file_is_not_device,
-		status_info_unavailable
+		status_info_unavailable = -1,
+		file_is_not_device = -2,
+		file_open_failed = -3,
+		device_capabilities_unavailable = -4,
+		device_format_unavailable = -5,
+		not_freed = -6,
+		device_video_capture_unsupported = -7,
+		device_streaming_unsupported = -8,
+		invalid_format_type = -9,
+		format_unsupported = -10,
+		device_buffer_request_failed = -11,
+		device_out_of_memory = -12,
+		user_out_of_memory = -13,
+		device_buffer_query_failed = -14,
+		mmap_failed = -15,
+		device_streaming_parameters_unavailable = -16,
+		device_set_streaming_parameters_failed = -17,
+		device_custom_timeperframe_unsupported = -18,
+		device_start_failed = -19,
+		device_queue_buffer_failed = -20,
+		poll_failed = -21,
+		stream_not_started = 1,
+		device_dequeue_buffer_failed = -22,
+		device_stop_failed = -23,
+		already_freed = -24,
+		munmap_failed = -25,
+		device_mmap_unsupported = -26,
+		device_request_buffers_failed = -27,
+		already_closed = -28,
+		file_close_failed = -29
 	};
-
-	static const char* getErrorMessage(Error cameraError, int errnoValue);
 
 	const char* deviceName;
 	int fd;
+
+	struct pollfd pollStruct;
 
 	struct v4l2_capability capabilities;					// NOTE: Struct is included so as to know that v4l2_* is a struct. Design choice.
 	struct v4l2_format format;
 	
 	struct v4l2_requestbuffers bufferMetadata;
+	uint32_t lastBufferIndex;
 	bool initialized = false;
 
 	struct v4l2_buffer bufferData;
 
-	struct v4l2_streamparm streamingParameters;
-
-	struct pollfd pollStruct;
-
-	// TODO: maybe add ifdefs around this getErrorMessage stuff in case the user doesn't want all of that const char* storage in his executable.
-	const char* getErrorMessage(Error cameraError, int errno);
-
 	struct BufferLocation { void* start; size_t size; bool queued; }* frameLocations;
+
+	struct v4l2_streamparm streamingParameters;
 
 	Camera(const char* deviceName);
 
@@ -110,31 +80,58 @@ public:
 	// Asks the device if the format in the format struct is acceptable. If it isn't, device changes format struct to nearest valid configuration. If it is, format struct stays the way it is.
 	Error tryFormat();
 
+	// initialization must be performed after opening device
+
 	// Try to use format data in format struct to initialize device format. Also initialize device buffers and initialize shared memory access using mmap.
 	Error init();
 	// TODO: Add function descriptions to the rest of all these, order them in the header and in the implementation files, and go through the whole thig with principles in mind, then make main.cpp acceptable and write a small test program. Save the test capture as ppm because it's easy.
+	// Same function as init(), except that it reads the device's preferred format, changes the pixelformat and field options to the specified values, and then initializes the device with the resulting format.
 	Error init(uint32_t pixelFormat, uint32_t field);	// Needs to run after open().
+	// Same as init(uint32_t, uint32_t), except that it uses V4L2_PIX_FMT_RGB24 as pixelFormat and V4L2_FIELD_NONE as field.
 	Error defaultInit();
 
+	// reads streaming parameters from device and fills streamingParameters struct
 	Error readStreamingParameters();
 
+	// Time per frame functions need to be called after opening device, but can be called before initializing device.
+	// Depending on the device, you may be able to call time per frame functions after starting stream as well.
+
+	// Try to set device's time per frame. Device might round to nearest acceptable value if one passes in a value that is deemed unacceptable.
 	Error setTimePerFrame(uint32_t numerator, uint32_t denominator);			// Needs to run after open(), can run before init().
 	Error getTimePerFrame(uint32_t& numerator, uint32_t& denominator);			// getFPS only yields round FPS values. If FPS is non-integer, rounds down.
 
-	Error start();					// Needs to run after open() and after init(). TODO: See if setFPS can be run after start correctly.
+	// Start streaming. Needs to be called after opening and initializing the device.
+	Error start();
 
+	// Frames can be queued before calling start(), they just won't get filled before calling start().
+	// dequeueFrame() and dequeueAllFrames() will infinitely hang if called before start(). dequeueAllQueuedFrames() won't unless frames were queued before start().
+
+	// Queue the frame at bufferData.index. Increments bufferData.index.
+	Error queueFrame();
+
+	// Dequeue the frame that was finished the earliest. Sets bufferData.index to the index of the newly dequeued frame.
+	Error dequeueFrame();
+
+	// Queue all frames. bufferData.index equals 0 after function returns.
 	Error queueAllFrames();
+
+	// Dequeue all frames. bufferData.index is set to the index of the last frame that was dequeued. Infinitely hangs if one of the frames wasn't queued.
 	Error dequeueAllFrames();
 
+	// Dequeues only frames that were queued and thereby doesn't hang because of unqueued frames. This advantage over dequeueAllFrames() comes at the cost of more computation.
 	Error dequeueAllQueuedFrames();
 
+	// Shoot a single (new) frame. The resulting frame is as recent as possible. Calls dequeueAllQueuedFrames(), then queueFrame() and then dequeueFrame(), so it's not cheap.
 	Error shootFrame();
 
+	// Stop streaming. All frames that haven't been dequeued yet are lost. Needs to be called after start().
 	Error stop();
 
+	// Free device and user resources. This function is the counterpart to init(). Stops the stream if that hasn't been done already.
 	Error free();
 
-	Error close();					// Calls free() before closing, so don't call both.
+	// Close device. This function is the counterpart to open(). Calls free().
+	Error close();
 
-	~Camera();
+	~Camera();			// calls close()
 };
